2fb24cb3d22efaa3574fb7f4b002ac3c2c2bc411
Young <y0ungartzy@gmail.com>
2021-09-22 15:36:15 +0630

    shopping-web

---
+++ src/containers/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="ui menu fixed">
+      <div className="ui center container">
+        <h2>Shopper</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
